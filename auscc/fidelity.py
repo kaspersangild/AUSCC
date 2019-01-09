f7df99d92c0f370fc198912e6c78fcde9f88c87a
kaspersangild <kaspersangild@gmail.com>
2019-01-09 17:01:57 +0100

    Added average fidelity+leakage calculators did some tests

--- auscc/fidelity.py
+++ auscc/fidelity.py
@@ -1,4 +1,54 @@
-import auscc
-def average_fidelity():
-    print('Hello!')
-    return 0
+import qutip as qt
+import numpy as np
+
+def build_unitary_basis(subspace_basis):
+    d = len(subspace_basis)
+    X = sum([psi1 * psi2.dag() for psi1,psi2 in zip(subspace_basis, np.roll(subspace_basis, 1))])
+    Z = sum([np.exp(2*np.pi*1j*n/d)*psi*psi.dag() for n,psi in enumerate(subspace_basis)])
+    u_basis = []
+    for l in range(d):
+        for k in range(d):
+            u_basis.append(X**k * Z**l)
+    return u_basis
+
+def build_pure_state_basis(subspace_basis, u_basis):
+    d = len(subspace_basis)
+    rho_basis = []
+    ket_basis = []
+    for k in range(d):
+        for l in range(k,d):
+            if k == l:
+                ket_basis.append(subspace_basis[k])
+            else:
+                ket_basis.append((subspace_basis[k] + subspace_basis[l])/np.sqrt(2))
+                ket_basis.append((subspace_basis[k] + 1j*subspace_basis[l])/np.sqrt(2))
+    Tinv = np.zeros((d**2,d**2), dtype=complex)
+    for k,ket in enumerate(ket_basis):
+        for j,u in enumerate(u_basis):
+            Tinv[j][k] = (u.dag() * qt.ket2dm(ket)).tr()/d
+    return ket_basis, np.linalg.inv(Tinv)
+
+def leakage(process, subspace_basis, tlist):
+    d = len(subspace_basis)
+    I_subspace = sum([qt.ket2dm(ket) for ket in subspace_basis])
+    return [1 - e for e in process(I_subspace/d, tlist, I_subspace)]
+
+def entanglement_fidelity_term(k, process, pure_state_basis, tlist, u_basis, d, T):
+    e_ops_k = sum([T[k][j]*u.dag() / d**3 for j,u in enumerate(u_basis)])
+    return process(pure_state_basis[k], tlist, e_ops_k)
+
+def entanglement_fidelity(process, subspace_basis, tlist, progress_bar=False):
+    d = len(subspace_basis)
+    u_basis = build_unitary_basis(subspace_basis)
+    pure_state_basis, T = build_pure_state_basis(subspace_basis, u_basis)
+    if progress_bar:
+        F_e =np.sum(qt.parallel_map(entanglement_fidelity_term, range(d**2), (process, pure_state_basis, tlist, u_basis, d, T), progress_bar=True), axis=0)
+    else:
+        F_e =np.sum(qt.parallel_map(entanglement_fidelity_term, range(d**2), (process, pure_state_basis, tlist, u_basis, d, T)), axis=0)
+    return F_e
+
+def average_fidelity(process, subspace_basis,  tlist, progress_bar=False):
+    d = len(subspace_basis)
+    L = leakage(process, subspace_basis, tlist)
+    F_e = entanglement_fidelity(process, subspace_basis, tlist)
+    return [(d*f_e + 1 - l) / (d + 1) for f_e,l in zip(F_e, L)]
--- auscc/tests/fidelity_test.py
+++ auscc/tests/fidelity_test.py
@@ -1,1 +1,64 @@
-from ..fidelity import *
+from auscc.fidelity import average_fidelity, entanglement_fidelity, leakage
+import qutip as qt
+import numpy as np
+
+if __name__ == '__main__':
+    # One qubit simple test unitary basis
+    basis = [qt.ket([0],[2]), qt.ket([1],[2])]
+    U_target = -1j*qt.sigmax()
+    H = qt.sigmax()
+    def process(rho, tlist, e_ops):
+        res = qt.mesolve(H, rho, tlist, e_ops = U_target*e_ops*U_target.dag())
+        return res.expect[0]
+
+    tlist = np.linspace(0,np.pi/2,10)
+    F_av = average_fidelity(process, basis, tlist)
+    print(F_av,'\n')
+    # One qubit with third state [0,1] comp basis + some noise
+    def projection_operator(subspace_basis, subspace_dims):
+        d = len(subspace_basis)
+        for j,ket in enumerate(subspace_basis):
+            ket_subspace = qt.basis(d, j)
+            if j == 0:
+                P = ket_subspace*ket.dag()
+            else:
+                P+= ket_subspace*ket.dag()
+        P.dims[0] = subspace_dims
+        return P
+    subspace_basis =  [qt.ket([0], 3), qt.ket([1], 3) ]
+    P = projection_operator(subspace_basis, [2])
+    U_target = P.dag()*(-1j*qt.sigmax())*P
+    H = qt.create(3)+qt.destroy(3)+20*qt.ket([2],[3])*qt.bra([2],[3])
+    def process(rho, tlist, e_ops):
+        res = qt.mesolve(H, rho, tlist,c_ops = [0.1*qt.destroy(3)] , e_ops = U_target*e_ops*U_target.dag())
+        return res.expect[0]
+    tlist = np.linspace(0,np.pi/2,10)
+    F_av = average_fidelity(process, subspace_basis, tlist)
+    print(F_av,'\n')
+    # One qudit with four states [1,2] comp basis
+    subspace_basis =  [qt.ket([1], 4), qt.ket([2], 4) ]
+    P = projection_operator(subspace_basis, [2])
+    U_target = P.dag()*(-1j*qt.sigmax())*P
+    H = qt.create(4)+qt.destroy(4)+20*qt.ket([3],[4])*qt.bra([3],[4])+20*qt.ket([0],[4])*qt.bra([0],[4])
+    def process(rho, tlist, e_ops):
+        res = qt.mesolve(H, rho, tlist, e_ops = U_target*e_ops*U_target.dag())
+        return res.expect[0]
+    tlist = np.linspace(0,np.pi/(2*np.sqrt(2)),10)
+    F_av = average_fidelity(process, subspace_basis, tlist)
+    print(F_av,'\n')
+    # One qubit and one qutrit + time dependent hamiltonian (CZ gate)
+    # subspace_basis =  [qt.ket(seq, [2,3]) for seq in qt.state_number_enumerate([2,2])]
+    # P = projection_operator(subspace_basis, [2,2])
+    # U_target_I = (-qt.tensor(qt.sigmaz(),qt.sigmaz())+qt.tensor(qt.sigmaz(),qt.qeye(2))+qt.tensor(qt.qeye(2),qt.sigmaz())+1)/2
+    # U_target_I =
+    # args = {'omega':100, 'A' : np.sqrt(2)}
+    # H0 = args['omega']*(qt.tensor(qt.num(2),qt.num(3)) + qt.tensor(qt.qeye(2),qt.num(3)**2))
+    # H0_comp = args['omega']*(qt.tensor(qt.num(2),qt.num(2)) + qt.tensor(qt.qeye(2), qt.num(2)**2))
+    # tlist = np.linspace(0,np.pi,10)
+    # U_target_S = []
+    # for t in tlist:
+    #     U_target_S.append((-1j*t*H0_comp).expm()*U_target_I*(1j*0*H0_comp).expm())
+    # H1 = [qt.tensor(qt.qeye(2),qt.create(3)+qt.destroy(3)),'A*cos(4*omega*t)']
+    # H = [H0,H1]
+    # F_av = average_fidelity(U_target_S, H, tlist, c_ops = [], args = args,basis = 'unitary',progress_bar = True)
+    # print(F_av)
